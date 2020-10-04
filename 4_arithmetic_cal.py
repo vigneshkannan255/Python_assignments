9873b407980a912308bd78c674a80abe5ca32d67
DevaRDev <52148841+DevaRDev@users.noreply.github.com>
2020-10-04 15:17:27 +0530

    Update 4_arithmetic_cal.py
    Comments added

--- 4_arithmetic_cal.py
+++ 4_arithmetic_cal.py
@@ -1,6 +1,7 @@
+# Program make a simple calculator
 a=0
 while(a=0)
-	choice=input('1.ADD\n2.SUB\n3.MUL\n4.DIV\n5.Exit')
+	choice=input('1.ADD\n2.SUB\n3.MUL\n4.DIV\n5.Exit') # Take input from the user
 	x=input('Enter A:')
 	y=input('Enter B:')	
 	if(choice == 1):
